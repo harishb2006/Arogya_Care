36606191b8d736341152622afb09a8729349a910
harishb2006 <imharishba@gmail.com>
2026-04-23 16:43:29 +0530

    Feat:completed admin login and dash initialize

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AdminLogin from './app/pages/AdminLogin';
+import AdminDashboard from './components/AdminDashboard';
+import './index.css';
 
 const App = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
+  const [token, setToken] = useState<string | null>(localStorage.getItem('adminToken'));
 
-export default App
+  const handleLoginSuccess = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem('adminToken', newToken);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.removeItem('adminToken');
+  };
+
+  return (
+    <>
+      {!token ? (
+        <AdminLogin onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <AdminDashboard onLogout={handleLogout} />
+      )}
+    </>
+  );
+};
+
+export default App;
--- server/main.py
+++ server/main.py
@@ -1,6 +1,22 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from routes import admin
+
+load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(admin.router)
 
 @app.get("/")
 def home():
